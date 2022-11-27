06301d028d0af977cdd85083b584cc7de8641b9f
Matijast <matijastipcevic01@yahoo.com>
2022-11-27 17:55:49 +0000

    remove your sky surprise

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import requests
 
 api_key = st.secrets["APIKEY"]
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-
 request1 = requests.get(url)
 content = request1.json()
 
 image_url = content["hdurl"]
 explanation = content["explanation"]
+
+if not explanation.find("Your Sky Surprise:") == -1:
+    explanation = explanation[:explanation.find("Your Sky Surprise:")]
 
 request2 = requests.get(image_url)
 image = request2.content
