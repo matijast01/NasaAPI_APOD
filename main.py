9d422b5c676bee656032b8a1db75d6aa82d9bddb
Matijast <matijastipcevic01@yahoo.com>
2022-11-27 17:18:26 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import requests
+
+api_key = st.secrets("APIKEY")
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+
+request1 = requests.get(url)
+content = request1.json()
+
+image_url = content["hdurl"]
+explanation = content["explanation"]
+
+request2 = requests.get(image_url)
+image = request2.content
+
+st.image(image)
+st.write(explanation)
